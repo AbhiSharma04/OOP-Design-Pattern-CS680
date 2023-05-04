8938c558b1d39b1a485073fe9d94379ce08e5798
AbhiSharma04 <114906591+AbhiSharma04@users.noreply.github.com>
2023-05-04 16:18:35 -0400

    Delete HW09 directory

--- HW09/src/edu/umb/cs680/hw09/SecurityContext/Authenticator.java
+++
@@ -1,8 +0,0 @@
-package edu.umb.cs680.hw09.SecurityContext;
-
-public class Authenticator {
-    public static boolean authenticate(SecurityContext contxt , EncryptedString pass)
-    {
-        return true;
-    }
-}
--- HW09/src/edu/umb/cs680/hw09/SecurityContext/State.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.hw09.SecurityContext;
-
-public interface State {
-    public State login(EncryptedString pass);
-    public State logout();
-
-}
